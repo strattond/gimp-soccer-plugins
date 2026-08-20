61ab6dc5e5f486ebb7610cf4bc086c661adb2a84
Daniel Stratton <dsstratton@outlook.com>
2026-08-21 07:56:48 +1000

    Code cleanups.  Golden boot done via MatPlotLib in squadi analysis now instead

--- fitba.py
+++ fitba.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import gi
+
 gi.require_version('Gimp', '3.0')
 gi.require_version('GimpUi', '3.0')
-from gi.repository import Gimp, Gio, GimpUi, GLib, Gtk
-
-import sys
 import json
-import math
+import sys
+
+from gi.repository import Gimp, GimpUi, Gio, GLib, Gtk
 
 score_table_proc  = "plug-in-football-tables"
-golden_boot_proc  = "plug-in-football-golden-boot"
 next_fixture_prc  = "plug-in-football-fixture"
 team_fixture_prc  = "plug-in-football-fixture-team"
 draw_football_pr  = "plug-in-football-shape"
@@ -80,7 +78,7 @@ def create_image_layer_at( image, folder, club, parentLayer, squareSize, posX, p
 # Extracts the columnar data
 def extract_column(data, attr):
   if not data:
-    return
+    return []
   return [str(record[attr]) for record in data]
 
 def coords_to_vec2_list(flat_coords):
@@ -143,86 +141,30 @@ def process_table( parLayer, image, data, xPos, yPos, ptSize):
   x_offset = 200  # Horizontal spacing
   target_name = "Oxley United FC"  # Name to highlight
 
+  div = data['div']
+  table = data['table']
   # Find row index for the target name
-  row_index = next((i for i, entry in enumerate(data["table"]) if entry["Team"] == target_name), None)
-  print(row_index)
+  row_index = next((i for i, entry in enumerate(table) if entry["Team"] == target_name), None)
   
   font = Gimp.Font.get_by_name("Serif")
   # Loop through fields to create text layers
   runningOffset = x_offset
-  grpLayer = Gimp.GroupLayer.new(image, data["comp"] + " Table")
+  grpLayer = Gimp.GroupLayer.new(image, div['name'] + " Table")
   image.insert_layer( grpLayer, parLayer, -1 )
   lHeight = 0
-  create_title_card(image, grpLayer, ptSize, font, data["comp"])
+  create_title_card(image, grpLayer, ptSize, font, div['name'])
   for i, field in enumerate(fields):
-    field_values = "\n".join( [labels[i]] + extract_column( data["table"], field ) )
+    field_values = "\n".join( [labels[i]] + extract_column( table, field ) )
     text_layer = create_text_layer_at( image, field_values, font, ptSize, grpLayer, xPos + runningOffset, yPos )
     runningOffset += offsets[i]
     lHeight = text_layer.get_height()
 
   if row_index is not None:
-    numRecords = len(data["table"]) + 1
+    numRecords = len(table) + 1
     rowSlice = lHeight / numRecords
     rowOffset_y = rowSlice * (row_index + 1)
     rowBottom_y = rowSlice * (row_index + 2)
     create_highlight_row( image, grpLayer, xPos, yPos + rowOffset_y, runningOffset + (ptSize * 20), rowBottom_y - rowOffset_y)
-  return image
-
-def process_gb_table(image, data, xPos, yPos, ptSize):
-
-  nPlayers = len(data)
-  firstE = data[0]
-  nRounds = len(firstE["scores"])
-  offsets = [None] * (nRounds + 1)
-  labels  = [None] * (nRounds + 1)
-  labels[0] = 'Player'
-  offsets[0] = 1000
-  for i in range(nRounds):
-    labels[i + 1] = str(i + 1)
-    offsets[i + 1] = 250
-  runningOffset = 200
-  font = Gimp.Font.get_by_name("Serif")
-  # Loop through fields to create text layers
-  grpLayer = Gimp.GroupLayer.new(image, "Golden Boot Table")
-  image.insert_layer( grpLayer, None, 0 )
-  create_title_card(image, grpLayer, ptSize, font, "Metro Golden Boot")
-  name_values = "\n".join( [labels[0]] + extract_column( data, "name" ) )
-  text_layer = create_text_layer_at( image, name_values, font, ptSize, grpLayer, xPos + runningOffset, yPos )
-  runningOffset += offsets[0]
-  
-  round_layer = create_title_card(image, grpLayer, ptSize / 2, font, "Match")
-  round_layer.set_offsets( xPos + runningOffset, yPos - round_layer.get_height())
-  
-  for i in range(nRounds):
-    nth_scores = [str(entry["scores"][i]) for entry in data if i < len(entry["scores"])]
-    nth_totals = [str(entry["running_total"][i]) for entry in data if i < len(entry["running_total"])]
-    field_values = labels[i + 1] + "\n"
-    for j in range(len(nth_scores)):
-      #field_values += nth_scores[j] + " (" + nth_totals[j] + ")\n"
-      #field_values += nth_scores[j] + "\n"
-      if nth_scores[j] != '0':
-        field_values += nth_scores[j] + "\n"
-      else:
-        field_values += "\n"
-        
-    text_layer = create_text_layer_at( image, field_values, font, ptSize, grpLayer, xPos + runningOffset, yPos )
-    runningOffset += offsets[i+1]
-
-  colors = [ "FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF"]
-  name_height = (text_layer.get_height() - 100) / (nPlayers+1)
-  for i in range(nPlayers):
-    create_player_colour( image, grpLayer, 200, yPos + (name_height) * (i+1), 100, 100, colors[i] )
-
-  drawLayer = Gimp.Layer.new(image, "Golden Boot Graph Drawing", image.get_width(), image.get_height(), Gimp.ImageType.RGBA_IMAGE, 100, Gimp.LayerMode.NORMAL)
-  image.insert_layer( drawLayer, grpLayer, -1 )
-
-  orgLineWith = Gimp.context_get_brush_size()
-  brushSize = 15
-  Gimp.context_set_brush_size( brushSize )
-  Gimp.pencil( drawLayer, [xPos, yPos + name_height - 25, xPos + runningOffset, yPos + name_height - 25])
-  Gimp.pencil( drawLayer, [xPos + 1100, yPos, xPos + 1100, yPos + text_layer.get_height() - name_height] )
-  Gimp.context_set_brush_size( orgLineWith )
-    
   return image
 
 def load_json(filename):
@@ -256,100 +198,6 @@ def score_table_run(procedure, run_mode, image, drawables, config, data):
   else:
     return procedure.new_return_values( Gimp.PDBStatusType.CANCEL, GLib.Error() )
 
-def draw_gb_graph(image, data, xPos, yPos, ptSize):
-  
-  graphHeight = 2000
-  brushSize   = 30
-  firstE      = data[0]
-  nRounds     = len(firstE["scores"])
-  bottom      = yPos + graphHeight
-  rightH      = image.get_width() - xPos
-  tickX       = (rightH - xPos) / (nRounds + 1)
-  nGoals      = firstE["goals"]
-  font        = Gimp.Font.get_by_name("Serif")
-  
-  orgLineWith = Gimp.context_get_brush_size()
-  Gimp.context_set_brush_size( brushSize )
-  grpLayer = Gimp.GroupLayer.new(image, "Golden Boot Graph")
-  image.insert_layer( grpLayer, None, 0 )
-  drawLayer = Gimp.Layer.new(image, "Golden Boot Graph Drawing", image.get_width(), image.get_height(), Gimp.ImageType.RGBA_IMAGE, 100, Gimp.LayerMode.NORMAL)
-  image.insert_layer( drawLayer, grpLayer, -1 )
-  
-  # Axis
-  Gimp.pencil( drawLayer, [xPos, yPos, xPos, bottom, rightH, bottom])
-  # X-Axis Ticks
-  for i in range(nRounds):
-    nextX = xPos + ( (i + 1) * tickX)
-    Gimp.pencil( drawLayer, [nextX, bottom, nextX, bottom - 100])
-  # Y-Axis ticks
-  tickY = graphHeight / (nGoals+1)
-  for i in range(nGoals):
-    nextY = yPos + ((i + 1) * tickY)
-    Gimp.pencil( drawLayer, [xPos, nextY, xPos + 100, nextY])
-  # Y-Axis Labels
-  for i in range(nGoals):
-    nextY = yPos + ((i + 1) * tickY)
-    label_value = str(nGoals - i)
-    text_layer = create_text_layer_at( image, label_value, font, ptSize, grpLayer, 250, nextY - text_layer.get_height() / 2 )
-    
-  # X-Axis Labels
-  for i in range(nRounds):
-    nextX = xPos + ( (i + 1) * tickX)
-    label_value = str(i + 1)
-    text_layer = create_text_layer_at( image, label_value, font, ptSize, grpLayer, nextX - text_layer.get_width() / 2, bottom + 50 )
-    
-  # Player Graph
-  curFG = Gimp.context_get_foreground()
-  colors = [ "FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF"]
-  nPlayers = len(data)
-  for i in range(nPlayers, 0, -1):
-    # Per player, lowest first, so higher scored players "overlay" lower ones
-    entry = data[i - 1]
-    bl = [xPos, bottom]
-    newFG = Gimp.color_parse_hex( colors[i - 1] )
-   # Bottom left to round 1 cumulative, round 1 cumulative to round 2 cumulative, etc
-    linecoords = [bl[0], bl[1]]
-    for j in range(nRounds):
-      goal = (entry["running_total"])[j]
-      linecoords.append( xPos + (j + 1) * tickX)
-      offset = (brushSize/2) * (nPlayers - (i+1))
-      gOffset = goal * tickY
-      linecoords.append( bottom - (gOffset + offset))
-    
-    print(linecoords)
-    Gimp.context_set_foreground( newFG )
-    Gimp.airbrush( drawLayer, 50, linecoords )
-  
-  Gimp.context_set_brush_size( orgLineWith )
-  Gimp.context_set_foreground( curFG )
-
-  return image
-
-def golden_boot_run(procedure, run_mode, image, drawables, config, data):
-
-  # Read JSON
-  data = load_json("D:\\Media\\Oxley\\squadi\\goldenboot.json")
-  
-  for entry in data:
-    entry["goals"] = sum(entry["scores"])
-    entry["running_total"] = []
-    temp_sum = 0
-    for score in entry["scores"]:
-      temp_sum += score
-      entry["running_total"].append(temp_sum)
-  
-  sorted_data = sorted( data, key=lambda x: (-x["goals"], x["name"]) )
-  
-  top5 = sorted_data[:5]
-  print( top5 )
-
-  image.undo_group_start()
-  process_gb_table( image, top5, 200, 600, 28 )
-  draw_gb_graph( image, top5, 400, 1800, 28 )
-  image.undo_group_end()
-
-  return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, None)
-
 def newDimensions(layer, newMaxDim):
   
   yVal = layer.get_height()
@@ -363,21 +211,23 @@ def newDimensions(layer, newMaxDim):
     divisor = newMaxDim / xVal
     return [ newMaxDim, yVal * divisor]
 
-def process_fixture_table(image, run_mode, folder, round, fixtures, ptSize ):
+def process_fixture_table(image, run_mode, folder, fixtures, ptSize ):
 
-  nRounds = len(fixtures)
   font = Gimp.Font.get_by_name("Serif")
   # Loop through fields to create text layers
   grpLayer = Gimp.GroupLayer.new(image, "Fixtures")
   image.insert_layer( grpLayer, None, 0 )
-  round_layer = create_title_card(image, grpLayer, ptSize * 2 / 3, font, "Round " + str(round))
+  round_layer = create_title_card(image, grpLayer, ptSize * 2 / 3, font, "Upcoming fixtures" )
   _,xPos,_ = round_layer.get_offsets()
   round_layer.set_offsets( xPos, 675 )
   
   i = 0
   for entry in fixtures:
     
-    grpLayerTeam = Gimp.GroupLayer.new( image, entry["div"] )
+    div = entry['div']
+    match = entry['match']
+    
+    grpLayerTeam = Gimp.GroupLayer.new( image, div['name'] )
     image.insert_layer( grpLayerTeam, grpLayer, 0 )
     
     #
@@ -389,70 +239,70 @@ def process_fixture_table(image, run_mode, folder, round, fixtures, ptSize ):
     #   "ground": "Elder Oval, Field 1"
     # },
     #
-    text_value = entry["div"] + " - " + entry["when"] + "\n" + entry["ground"] + " vs "
-    if entry["home"] != "Oxley United FC":
-      text_value += entry["home"]
+    text_value = div['name'] + " - " + match["when"] + "\n" + match["ground"] + " vs "
+    if match["home"] != "Oxley United FC":
+      text_value += match["home"]
     else:
-      text_value += entry["away"]
+      text_value += match["away"]
       
-    text_layer = create_text_layer_at( image, text_value, font, ptSize, grpLayerTeam, 1500, 1000 + i * 500 )
+    create_text_layer_at( image, text_value, font, ptSize, grpLayerTeam, 1500, 1000 + i * 500 )
 
     # Load home    
-    file = Gio.File.new_for_path( folder + "\\" + entry["home"] + ".png" )
+    file = Gio.File.new_for_path( folder + "\\" + match["home"] + ".png" )
     hlayer = Gimp.file_load_layer(Gimp.RunMode.NONINTERACTIVE, image, file)
     image.insert_layer(hlayer, grpLayerTeam, -1 )
     hX, hY = newDimensions( hlayer, 400 )
     hlayer.scale( hX, hY, True )
     hlayer.set_offsets( 500, 1000 + i * 500 )
 
-    file = Gio.File.new_for_path( folder + "\\" + entry["away"] + ".png" )
+    file = Gio.File.new_for_path( folder + "\\" + match["away"] + ".png" )
     alayer = Gimp.file_load_layer(Gimp.RunMode.NONINTERACTIVE, image, file)
     image.insert_layer(alayer, grpLayerTeam, -1 )
     aX, aY = newDimensions( alayer, 400 )
     alayer.scale( aX, aY, True )
     alayer.set_offsets( 5000, 1000 + i * 500 )
 
-
     i = i + 1
   return image
 
-def process_fixture_results(image, run_mode, folder, round, fixtures, ptSize ):
+def process_fixture_results( image, run_mode, folder, fixtures, ptSize ):
 
-  nRounds = len(fixtures)
   font = Gimp.Font.get_by_name("Serif")
   # Loop through fields to create text layers
   grpLayer = Gimp.GroupLayer.new(image, "Results")
   image.insert_layer( grpLayer, None, 0 )
-  round_layer = create_title_card(image, grpLayer, ptSize * 2 / 3, font, "Round " + str(round))
+  round_layer = create_title_card(image, grpLayer, ptSize * 2 / 3, font, "Latest Results" )
   _,xPos,_ = round_layer.get_offsets()
   round_layer.set_offsets( xPos, 675 )
   
   i = 0
   for entry in fixtures:
     
-    grpLayerTeam = Gimp.GroupLayer.new( image, entry["div"] )
+    div = entry['div']
+    match = entry['match']
+    
+    grpLayerTeam = Gimp.GroupLayer.new( image, div['name'] )
     image.insert_layer( grpLayerTeam, grpLayer, 0 )
     
-    create_text_layer_at( image, entry["div"],            font, ptSize, grpLayerTeam, 500, 1100 + i * 500 )
-    create_text_layer_at( image, entry["home"],           font, ptSize, grpLayerTeam, 1500, 1100 + i * 500 )
-    create_text_layer_at( image, str(entry["goalsHome"]), font, ptSize, grpLayerTeam, 2850, 1100 + i * 500 )
-    create_text_layer_at( image, str(entry["goalsAway"]), font, ptSize, grpLayerTeam, 3150, 1100 + i * 500 )
-    create_text_layer_at( image, entry["away"],           font, ptSize, grpLayerTeam, 3500, 1100 + i * 500 )
+    create_text_layer_at( image, div['name'],             font, ptSize, grpLayerTeam, 100, 1100 + i * 500 )
+    create_text_layer_at( image, match["home"],           font, ptSize, grpLayerTeam, 1500, 1100 + i * 500 )
+    create_text_layer_at( image, str(match["goalsHome"]), font, ptSize, grpLayerTeam, 2850, 1100 + i * 500 )
+    create_text_layer_at( image, str(match["goalsAway"]), font, ptSize, grpLayerTeam, 3150, 1100 + i * 500 )
+    create_text_layer_at( image, match["away"],           font, ptSize, grpLayerTeam, 3500, 1100 + i * 500 )
 
     # Load home
-    hlayer = create_image_layer_at( image, folder, entry["home"], grpLayerTeam, 400, 1000, 1000 + i * 500 )
-    alayer = create_image_layer_at( image, folder, entry["away"], grpLayerTeam, 400, 5000, 1000 + i * 500 )
+    hlayer = create_image_layer_at( image, folder, match["home"], grpLayerTeam, 400, 1000, 1000 + i * 500 )
+    alayer = create_image_layer_at( image, folder, match["away"], grpLayerTeam, 400, 5000, 1000 + i * 500 )
     
-    if entry["goalsHome"] < entry["goalsAway"]:
+    if match["goalsHome"] < match["goalsAway"]:
       hlayer.desaturate(Gimp.DesaturateMode.LUMINANCE)
-    elif entry["goalsAway"] < entry["goalsHome"]:
+    elif match["goalsAway"] < match["goalsHome"]:
       alayer.desaturate(Gimp.DesaturateMode.LUMINANCE)
     i = i + 1
   return image
 
-def process_team_fixture_table(image, folder, division, fixtures, ptSize ):
+def process_team_fixture_table( image, folder, division, fixtures, ptSize ):
 
-  nRounds = len(fixtures)
   font = Gimp.Font.get_by_name("Serif")
   # Loop through fields to create text layers
   grpLayer = Gimp.GroupLayer.new(image, "Fixtures " + division)
@@ -482,14 +332,13 @@ def process_team_fixture_table(image, folder, division, fixtures, ptSize ):
     else:
       text_value += entry["away"]
       
-    text_layer = create_text_layer_at( image, text_value, font, ptSize, grpLayerTeam, 2000, 1000 + i * 500 )
+    create_text_layer_at( image, text_value, font, ptSize, grpLayerTeam, 2000, 1000 + i * 500 )
 
     # Load home    
     file = Gio.File.new_for_path( folder + "\\" + entry["homeImage"] )
     hlayer = Gimp.file_load_layer(Gimp.RunMode.NONINTERACTIVE, image, file)
     image.insert_layer(hlayer, grpLayerTeam, -1 )
     hX, hY = newDimensions( hlayer, 400 )
-    #hlayer.resize( hX, hY, 0, 0 )
     hlayer.scale( hX, hY, True )
     hlayer.set_offsets( 1500, 1000 + i * 500 )
 
@@ -497,8 +346,7 @@ def process_team_fixture_table(image, folder, division, fixtures, ptSize ):
     alayer = Gimp.file_load_layer(Gimp.RunMode.NONINTERACTIVE, image, file)
     image.insert_layer(alayer, grpLayerTeam, -1 )
     aX, aY = newDimensions( alayer, 400 )
-    #alayer.resize( aX, aY, 0, 0 )
-    alayer.scale( hX, hY, True )
+    alayer.scale( aX, aY, True )
     alayer.set_offsets( 4500, 1000 + i * 500 )
 
     i = i + 1
@@ -526,7 +374,7 @@ def process_infographic(image, run_mode, folder, data, ptSize ):
     str(data["goals"]) + " " + ball,
     str(data["uniqueScorers"]) + " " + person,
     data["top_scorer"]["name"] + " with " + str( data["top_scorer"]["goals"] ) + " goals",
-    data["avgGoalsPerRound"],
+    f"{data["avgGoalsPerRound"]:.2f}",
     str(data["highestRoundGoals"]) + " in round " + str( data["highestRound"] )
   ]
 
@@ -540,7 +388,7 @@ def process_infographic(image, run_mode, folder, data, ptSize ):
   create_text_layer_at( image, "Cards", font, ptSize * 2, grpLayer, 500, secondLine + lineHeight )
   create_card( image, grpLayer, 1500, secondLine + lineHeight, 200, 400 )
   dataEntries = [
-    str(data["cards"]),
+    str(data["yellows"]),
     str(data["uniqueCarders"]) + " " + person,
     data["top_carder"]["name"] + " with " + str( data["top_carder"]["cards"] ) + " cards"
   ]
@@ -560,11 +408,9 @@ def fixture_run(procedure, run_mode, image, drawables, config, data):
     fixData = load_json( json_path )
     
     logoFolder = "D:\\Media\\Oxley\\ClubLogos"
-    round = fixData["round"]
-    fixtures = fixData["fixtures"]
 
     image.undo_group_start()
-    process_fixture_table( image, run_mode, logoFolder, round, fixtures, 32 )
+    process_fixture_table( image, run_mode, logoFolder, fixData, 32 )
     image.undo_group_end()
 
     return procedure.new_return_values( Gimp.PDBStatusType.SUCCESS, None )
@@ -580,11 +426,11 @@ def fixture_res_run(procedure, run_mode, image, drawables, config, data):
     fixData = load_json( json_path )
     
     logoFolder = "D:\\Media\\Oxley\\ClubLogos"
-    round = fixData["round"]
-    results = fixData["results"]
+
+    fixtures = [ entry for entry in fixData if (entry['match']['home'] != 'Bye' and entry['match']['away'] != 'Bye') ]
 
     image.undo_group_start()
-    process_fixture_results( image, run_mode, logoFolder, round, results, 32 )
+    process_fixture_results( image, run_mode, logoFolder, fixtures, 32 )
     image.undo_group_end()
 
     return procedure.new_return_values( Gimp.PDBStatusType.SUCCESS, None )
@@ -619,8 +465,6 @@ def draw_hexagon( layer, xOff, yOff, size ):
   linecoords = [ tL[0], tL[1], tR[0], tR[1], mR[0], mR[1], bR[0], bR[1], bL[0], bL[1], mL[0], mL[1], tL[0], tL[1] ]
   Gimp.pencil( layer, linecoords )
 
-  return None
-
 def draw_shape_run(procedure, run_mode, image, drawables, config, data):
 
   image.undo_group_start()
@@ -639,7 +483,6 @@ def draw_shape_run(procedure, run_mode, image, drawables, config, data):
   cX = 1024
   cY = 1024
   oddIncX = (2 * size) - (size / 2)
-  oddIncY = 0
   evnIncX = (3 * size) / 4
   evnIncY = (2 * size) - (size / 2)
 
@@ -710,7 +553,7 @@ def infographic_run(procedure, run_mode, image, drawables, config, data):
 class SoccerPlugin (Gimp.PlugIn):
   def do_query_procedures(self):
     print( "Query self procedures" )
-    return [ score_table_proc, golden_boot_proc, next_fixture_prc, team_fixture_prc, draw_football_pr, fixture_res_proc, infographic_proc ]
+    return [ score_table_proc, next_fixture_prc, team_fixture_prc, draw_football_pr, fixture_res_proc, infographic_proc ]
 
   def do_set_i18n (self, name):
       return False
@@ -720,9 +563,6 @@ class SoccerPlugin (Gimp.PlugIn):
 
     if name == score_table_proc:
       procedure = self.createProc(name, score_table_run, "Score Tables", '<Image>/Filters/Soccer/', "Score Tables" )
-      
-    if name == golden_boot_proc:
-      procedure = self.createProc(name, golden_boot_run, "Golden Boot", '<Image>/Filters/Soccer/', "Golden Boot" )
       
     if name == next_fixture_prc:
       procedure = self.createProc(name, fixture_run, "Layout fixtures", '<Image>/Filters/Soccer/', "Layout Fixtures" )
